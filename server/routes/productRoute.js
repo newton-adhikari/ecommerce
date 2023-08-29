b2e920ecf606e7df92415cb0a05dce2d6a56b707
Newton Adhikari <neadh072@kathmandu>
2023-08-29 15:50:11 +0545

    added routes for getting all and deleting reviews

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,14 +1,16 @@
 const productRoute = require("express").Router();
-const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails, createProductReview } = require("../controllers/productController");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails, createProductReview, getProductReviews, deleteProductReview } = require("../controllers/productController");
 const { isAuthenticated, authtorizeRoles } = require("../middleware/auth");
 
 productRoute.route("/products").get(isAuthenticated, getAllProducts);
 productRoute.route("/product/new").post(isAuthenticated, createProduct);
-productRoute.route("/product/review").put(isAuthenticated, createProductReview);
 productRoute
     .route("/product/:id")
     .put(isAuthenticated, updateProduct)
     .delete(isAuthenticated, deleteProduct)
     .get(getProductDetails);
+
+productRoute.route("/product/review").put(isAuthenticated, createProductReview);
+productRoute.route("/reviews").get(getProductReviews).delete(isAuthenticated, deleteProductReview);
 
 module.exports = productRoute;
