6c36259a7b473e97d0dd553ff6bd674ff1ca1667
Newton Adhikari <neadh072@kathmandu>
2023-08-29 09:35:40 +0545

    created route to update product review and create new review

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -75,3 +75,43 @@ exports.deleteProduct = catchAsyncError(async (req, res) => {
         message: "Product deleted successfully"
     })
 });
+
+exports.createProductReview = catchAsyncError(async(req, res, next) => {
+    const { rating, comment, productId } = req.body;
+    
+    const product = await Product.findById(productId);
+
+    const review = {
+        user: req.user._id,
+        rating: Number(rating),
+        comment
+    }
+
+    const alreadyReviewd = product.reviews.find(rev => rev.user._id.toString() === req.user._id.toString())
+
+    if(alreadyReviewd) {
+        product.reviews.forEach(rev => {
+            if (rev.user._id.toString() === req.user._id.toString()) {
+                rev.rating = rating;
+                rev.comment = comment
+            }
+        })
+    }
+    else {
+        // this is a new review with new rating
+        product.reviews.push(review);
+        product.numberOfReviews = product.reviews.length;
+    }
+
+    // calculate the updated rating for the product
+    let avg = 0;
+    product.reviews.forEach(rev => avg += rev.rating);
+
+    product.rating = avg/product.numberOfReviews;
+    await product.save({validateBeforeSave: false});
+
+    res.status(200).json({
+        success: true,
+        message: "product review created successfully"
+    })
+})
