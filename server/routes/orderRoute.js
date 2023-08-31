69e2339d79cf9070fc43fd616b2bc9715a3b262d
Newton Adhikari <neadh072@kathmandu>
2023-08-31 10:13:59 +0545

    finishing all crud routes for order

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,7 +1,11 @@
 const orderRoute          = require("express").Router();
-const { createOrder }     = require("../controllers/orderController");
-const { isAuthenticated } = require("../middleware/auth");
+const { createOrder, getSingleOrder, myOrders, getAllOrders, updateOrder, deleteOrder }     = require("../controllers/orderController");
+const { isAuthenticated, authtorizeRoles } = require("../middleware/auth");
 
 orderRoute.route("/order/new").post(isAuthenticated, createOrder);
+orderRoute.route("/order/:id").get(isAuthenticated, getSingleOrder);
+orderRoute.route("/orders/me").get(isAuthenticated, myOrders);
+orderRoute.route("/admin/orders").get(isAuthenticated, authtorizeRoles("admin"), getAllOrders);
+orderRoute.route("/admin/order/:id").put(isAuthenticated, authtorizeRoles("admin"), updateOrder).delete(isAuthenticated, authtorizeRoles("admin"), deleteOrder);
 
 module.exports = orderRoute;
